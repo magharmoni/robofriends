c503cb8b3064ca147fcdf0068a0963bf0aa9d0a4
Anton <anton@magharmoni.se>
2018-11-07 10:28:57 +0100

    deploy

---
+++ src/robots.js
@@ -0,0 +1,26 @@
+export const robots = [
+{
+	id: 1,
+	name: 'Anton Hansson',
+	username: 'Mutanton', 
+	email: 'anton1@magharmoni.se'
+},
+{
+	id: 2,
+	name: 'Elina Danielsson',
+	username: 'SnyggElina', 
+	email: 'Elina@gmail.com'
+},
+{
+	id: 3,
+	name: 'Hulken B Banner',
+	username: 'The Hulk', 
+	email: 'hulken@Smash.com'
+},
+{
+	id: 4,
+	name: 'Peter Parker',
+	username: 'Spider-Man', 
+	email: 'spider@spiderman.net'
+}
+];
