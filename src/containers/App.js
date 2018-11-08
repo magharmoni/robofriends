3ee3537cf31fa1a3f5ca8eaa5290a938ca614ae7
Anton <anton@magharmoni.se>
2018-11-08 09:21:23 +0100

    moved folder fron google drive to desktop

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,15 +1,32 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
 import './App.css';
 
+import { setSearchField } from '../actions';
+
+const mapStateToProps = state => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error		
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value))
+	}
+}
+
 class App extends Component {
 	constructor() {
 		super()
 		this.state = {
-			robots: [],
-			searchfield: ''
+			robots: []
 		}
 	}
 
@@ -22,20 +39,19 @@ class App extends Component {
 		})
 	}
 
-	onSearchChange = (event) => {
-		this.setState({ searchfield: event.target.value });
-	}
 
 	render() {
+		const { searchField, onSearchChange } = this.props;
 		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
+			// return robot.name.toLowerCase().includes(searchField.toLowerCase())
+			return robot.name.toLowerCase().includes(searchField.toLowerCase())
 		})
 		return !this.state.robots.length ? 
 		<h1>Loading</h1> : 
 		(
 			<div className='tc'>
 				<h1 className='f1'>RoboFriends</h1>
-				<SearchBox searchChange={this.onSearchChange}/>
+				<SearchBox searchChange={onSearchChange}/>
 				<Scroll>
 					<CardList robots={filteredRobots}/>
 				</Scroll>
@@ -44,4 +60,4 @@ class App extends Component {
 	}	
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
