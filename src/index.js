8fb499e515be3fc71472dbf3cd88c710d798ad39
magharmoni <anton@magharmoni.se>
2018-11-14 12:16:29 +0100

    new working version with redux

--- src/index.js
+++ src/index.js
@@ -1,29 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createStore, combineReducers, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createStore, applyMiddleware, combineReducers } from "redux";
-import { createLogger } from "redux-logger";
 import thunkMiddleware from 'redux-thunk';
-import './index.css';
-import App from './containers/App.js';
-import * as serviceWorker from './serviceWorker';
+import { createLogger } from 'redux-logger';
 import 'tachyons';
-import { searchRobots, requestRobots } from './reducers';
 
-const logger = createLogger();
+import App from './containers/App';
+import registerServiceWorker from './registerServiceWorker';
+import { requestRobots, searchRobots } from './reducers'
 
-const rootReducer = combineReducers({ searchRobots, requestRobots });
-const store = createStore(rootReducer, applyMiddleware(thunkMiddleware, logger))
-// const store = createStore(rootReducer)
+import './index.css';
 
+const logger = createLogger()
 
-ReactDOM.render(<Provider store={store}>
-                    <App />
-                </Provider>, document.getElementById('root'));
+const rootReducers = combineReducers({requestRobots, searchRobots})
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const store = createStore(rootReducers, applyMiddleware(thunkMiddleware, logger))
 
-
+ReactDOM.render(
+  <Provider store={store}>
+    <App/>
+  </Provider>,
+  document.getElementById('root')
+);
+registerServiceWorker();
--- src/reducers.js
+++ src/reducers.js
@@ -1,38 +1,37 @@
-import { 
-    CHANGE_SEARCH_FIELD,
+import {
+    CHANGE_SEARCHFIELD,
     REQUEST_ROBOTS_PENDING,
     REQUEST_ROBOTS_SUCCESS,
-    REQUEST_ROBOTS_FAILED 
-} from "./constants.js";
-
-const intitialStateSearch = {
+    REQUEST_ROBOTS_FAILED
+   } from './constants';
+  
+  const initialStateSearch = {
     searchField: ''
-}
-
-export const searchRobots = (state=intitialStateSearch, action={}) => {
-    switch(action.type) {
-        case CHANGE_SEARCH_FIELD:
-            return Object.assign({}, state, { searchField: action.payload })
-        default: 
-            return state;
+  }
+  
+  export const searchRobots = (state=initialStateSearch, action={}) => {
+    switch (action.type) {
+      case CHANGE_SEARCHFIELD:
+        return Object.assign({}, state, {searchField: action.payload})
+      default:
+        return state
     }
-}
-
-const initialStateRobots = {
-    isPending: false,
+  }
+  
+  const initialStateRobots = {
     robots: [],
-    error: ''
-}
-
-export const requestRobots = (state=initialStateRobots, action={}) => {
-    switch(action.type) {
-        case REQUEST_ROBOTS_PENDING:
-            return Object.assign({}, state, { isPending: true })
-        case REQUEST_ROBOTS_SUCCESS:
-            return Object.assign({}, state, { robots: action.payload, isPending: false })
-        case REQUEST_ROBOTS_FAILED:
-            return Object.assign({}, state, { error: action.payload, isPending: false })
-        default:
-        return state;
+    isPending: true
+  }
+  
+  export const requestRobots = (state=initialStateRobots, action={}) => {
+    switch (action.type) {
+      case REQUEST_ROBOTS_PENDING:
+        return Object.assign({}, state, {isPending: true})
+      case REQUEST_ROBOTS_SUCCESS:
+        return Object.assign({}, state, {robots: action.payload, isPending: false})
+      case REQUEST_ROBOTS_FAILED:
+        return Object.assign({}, state, {error: action.payload})
+      default:
+        return state
     }
-}
+  }
