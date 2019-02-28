eecb9ca9749b7d670c6a85742d31d7506a5501a2
Anton <anton@magharmoni.se>
2019-02-28 14:56:16 +0100

    added some test

---
+++ build/precache-manifest.0e4f91a8fd01f46763df2a92fb2a2ecb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0f994cc54076c67b1679",
+    "url": "/static/js/main.0f994cc5.chunk.js"
+  },
+  {
+    "revision": "28b68bd1af56d9e563d3",
+    "url": "/static/js/1.28b68bd1.chunk.js"
+  },
+  {
+    "revision": "0f994cc54076c67b1679",
+    "url": "/static/css/main.791eb078.chunk.css"
+  },
+  {
+    "revision": "28b68bd1af56d9e563d3",
+    "url": "/static/css/1.3d309e8b.chunk.css"
+  },
+  {
+    "revision": "d86f407390192c939b25202c237eef6e",
+    "url": "/index.html"
+  }
+];
